234f2de4cf537820c46f337ed208d132e039799a
smfssilva <smfssilva@yahoo.com>
2021-08-08 19:55:12 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -6,8 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import com.mysql.cj.xdevapi.SelectStatement;
-
 import db.DB;
 import db.DbException;
 import model.dao.SellerDao;
@@ -51,21 +49,13 @@ public class SellerDaoJDBC implements SellerDao{
                 "SELECT seller.*,department.Name as DepName "
                 +"FROM seller INNER JOIN department "
                 +"ON seller.DepartmentId = department.Id "
-                +"WHERE seller.Id = ?"); 
+                +"WHERE seller.Id = ?"
+            ); 
             st.setInt(1, id);
             rs = st.executeQuery();
             if (rs.next()) {
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
-
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setBaseSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthDate(rs.getDate("BirthDate"));
-                obj.setDepartment(dep);
+                Department dep = instatiateDepartment(rs);
+                Seller obj = instantiateSeller(rs, dep);
                 return obj;
             }
             return null;
@@ -77,6 +67,24 @@ public class SellerDaoJDBC implements SellerDao{
             DB.closeResltSet(rs);
         }
 
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        obj.setDepartment(dep);
+        return obj;
+    }
+
+    private Department instatiateDepartment(ResultSet rs) throws SQLException {
+        Department dep =new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+        return dep;
     }
 
     @Override
