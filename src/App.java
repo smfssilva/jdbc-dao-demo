538408ead12d01cf0c67d5bdda344d1f95180a83
smfssilva <smfssilva@yahoo.com>
2021-08-08 18:05:17 -0300

    Remove package Application

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
+import model.entities.Department;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+
     }
 }
