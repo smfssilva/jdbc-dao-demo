e619311f6c42fa93f7e0945054b11496514bf453
smfssilva <smfssilva@yahoo.com>
2021-08-08 21:16:21 -0300

    update implementation

--- src/App.java
+++ src/App.java
@@ -34,6 +34,11 @@ public class App {
         sellerDao.insert(newsSeller);
         System.out.println("Inserted! New id = " + newsSeller.getId());
 
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
         
     }
 }
